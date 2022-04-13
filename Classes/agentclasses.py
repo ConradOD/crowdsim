b5863bfcb0a09b3299ffc2c260121993a62b2d4f
Conrad O'Driscoll <101503093+ConradOD@users.noreply.github.com>
2022-04-13 13:30:05 +0100

    Tidied up static agentclasses
    Refined the code to have more suitable referencing and naming in order to make clear the transition from static to dyamic time step.

--- Classes/agentclasses.py
+++ Classes/agentclasses.py
@@ -23,17 +23,17 @@ class Static_Spectator_Static_Performer():
         self.radius = radius
         self.x = x_pos
         self.y = y_pos
-        self.pos = [x_pos, y_pos]
+        self.pos = [self.x, self.y]
         
         self.visibility = 1
-        self.r_length = math.sqrt((x_pos**2)+(y_pos**2))
-        if x_pos < 0 :
-            self.theta = math.pi - math.atan(y_pos/abs(x_pos))
-        elif x_pos == 0:
+        self.r_length = math.sqrt((self.x**2)+(self.y**2))
+        if self.x < 0 :
+            self.theta = math.pi - math.atan(self.y/abs(self.x))
+        elif self.x == 0:
             self.theta = math.pi / 2
         else:
             
-            self.theta = math.atan(y_pos/x_pos)
+            self.theta = math.atan(self.y/self.x)
         
     def obstruction_field(self, performer_pos, blocked_region_dataset):
         distance = math.dist(self.pos, performer_pos)
@@ -49,15 +49,13 @@ class Static_Spectator_Static_Performer():
     
     def obstructed_view(self, blocked_region_dataset):
         for blocked_region in blocked_region_dataset:
-            region_start_distance = blocked_region[0]
-            region_angle = blocked_region[1]
             
-            if self.r_length > (region_start_distance + 0.5)  and region_angle[0] <= self.theta <= region_angle[1] :
-                    self.visibility=0
+            if self.r_length > (blocked_region[0]+ 0.25)  and blocked_region[1][0] <= self.theta <= blocked_region[1][1] :
+                        self.visibility=0
+                        break
             
-            else: 
-                
-                continue
+            else:
+                        continue
             
     
         
